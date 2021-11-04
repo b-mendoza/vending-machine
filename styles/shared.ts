b58dd0ef910a2642429d6ad37952bfa1d46fa293
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-04 16:44:13 -0600

    feat: move styled components to `theme` directory

--- styles/shared.ts
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const StyledMachineWrapper = styled.section`
-  display: grid;
-
-  gap: 2rem;
-
-  grid-template-columns: repeat(4, 1fr);
-`;
---
+++ theme/shared.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const StyledMachineWrapper = styled.div`
+  display: grid;
+
+  gap: 2rem;
+
+  grid-template-columns: repeat(auto-fill, minmax(24rem, 1fr));
+`;
+
