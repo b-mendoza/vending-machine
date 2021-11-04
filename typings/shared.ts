5090becf17251bfbac9d5b5d8d72a396c64ee713
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-04 16:42:52 -0600

    feat: add `Breakpoints` enum

--- typings/shared.ts
+++ typings/shared.ts
@@ -4,6 +4,16 @@ export type APIResponse = {
   data: Product[];
 };
 
+export enum Breakpoints {
+  DesktopBreakpoint = '1200PX',
+  LargeDesktopBreakpoint = '1500PX',
+  LargeMobileBreakpoint = '576PX',
+  LargeTabletBreakpoint = '992PX',
+  MobileBreakpoint = '340PX',
+  SmallMobileBreakpoint = '280PX',
+  TabletBreakpoint = '768PX',
+}
+
 export type CustomPageProps = {
   pageProps: Record<string, unknown>;
 };
