56cf653eef4070ede7a35fe0adb5fc93a474f20f
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-04 16:42:36 -0600

    fix: add `domains` to Nextjs config

--- next.config.js
+++ next.config.js
@@ -6,5 +6,19 @@ module.exports = {
   env: {
     API_URL: 'https://vending-machine-test.vercel.app/api/products',
   },
+  images: {
+    domains: [
+      'assets.epicurious.com',
+      'images-na.ssl-images-amazon.com',
+      'images.ctfassets.net',
+      'images.heb.com',
+      'products3.imgix.drizly.com',
+      'randys-pizza.com',
+      'sc04.alicdn.com',
+      'upload.wikimedia.org',
+      'veohops.com',
+      'www.cobsbread.com',
+    ],
+  },
   reactStrictMode: true,
 };
