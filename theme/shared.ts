c25b6cfa9bda544715fd65438331e7b1f3011186
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-04 16:44:24 -0600

    feat: add `StyledContainer` styled component

--- theme/shared.ts
+++ theme/shared.ts
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 
+import { Breakpoints } from 'typings/shared';
+
 export const StyledMachineWrapper = styled.div`
   display: grid;
 
@@ -8,3 +10,14 @@ export const StyledMachineWrapper = styled.div`
   grid-template-columns: repeat(auto-fill, minmax(24rem, 1fr));
 `;
 
+export const StyledContainer = styled.section`
+  padding: 2rem;
+
+  margin: 0 auto;
+
+  max-width: 120rem;
+
+  @media (min-width: ${Breakpoints.MobileBreakpoint}) {
+    padding: 3rem;
+  }
+`;
