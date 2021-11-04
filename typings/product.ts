43390b85cfd7b487a48812689dbf36dc898bc8f9
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-04 15:47:23 -0600

    feat: update `products` types

--- typings/product.ts
+++ typings/product.ts
@@ -1,3 +1,8 @@
+export type NormalizedProduct = Omit<Product, 'preparation_time'> & {
+  isAvailable: boolean;
+  preparationTime: number;
+};
+
 export type Product = {
   id: string;
   name: string;
