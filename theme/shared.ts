9b0255f6f9f005e0966d8ef692af7e071dbe2e04
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-04 19:06:22 -0600

    feat: update location of `StyledMachineWrapper` styled component

--- theme/shared.ts
+++ theme/shared.ts
@@ -2,14 +2,6 @@ import styled from '@emotion/styled';
 
 import { Breakpoints } from 'typings/shared';
 
-export const StyledMachineWrapper = styled.div`
-  display: grid;
-
-  gap: 2rem;
-
-  grid-template-columns: repeat(auto-fill, minmax(24rem, 1fr));
-`;
-
 export const StyledContainer = styled.section`
   padding: 2rem;
 
