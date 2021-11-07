b029e6c88c70f7479d8c26279f0674e877ea581d
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-07 00:08:39 -0600

    feat: improve `SkeletonProduct` component

--- components/Skeletons/SkeletonProduct/index.tsx
+++ components/Skeletons/SkeletonProduct/index.tsx
@@ -1,33 +1,15 @@
 import { Skeleton } from 'antd';
 
+import SkeletonButton from '../SkeletonButton';
+
 import styles from './SkeletonProduct.module.css';
-
-const { Avatar } = Skeleton;
-
-const avatarStyles: React.CSSProperties = {
-  minHeight: '24rem',
-  height: 'inherit',
-  minWidth: '24rem',
-  width: 'inherit',
-};
-
-const titleStyles: React.CSSProperties = {
-  height: '2.2rem',
-  margin: 0,
-  marginTop: '1rem',
-};
 
 function SkeletonProduct() {
   return (
-    <article>
-      <Avatar
-        active
-        className={styles.avatarElement}
-        shape="square"
-        style={avatarStyles}
-      />
+    <article className={styles.skeleton}>
+      <Skeleton.Image className={styles.image__skeleton} />
 
-      <Skeleton active paragraph={false} title={{ style: titleStyles }} />
+      <SkeletonButton />
     </article>
   );
 }
