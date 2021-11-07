578cfa30eb6f7805891d16ca82f39bd90536953a
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-07 04:04:14 -0600

    test: add mock to API response

---
+++ __mocks__/apiResponse.ts
@@ -0,0 +1,91 @@
+export const apiResponse = {
+  data: [
+    {
+      id: '70021987-0535-462c-87e4-f6f26662447f',
+      name: 'Hamburger',
+      preparation_time: 85,
+      thumbnail:
+        'https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/pass/the-ultimate-hamburger.jpg',
+    },
+    {
+      id: '353e4f07-7228-4c66-a83e-b2bc46eb239c',
+      name: 'Pizza',
+      preparation_time: 50,
+      thumbnail:
+        'https://randys-pizza.com/wp-content/uploads/new-york-style-pizza-and-subs.png',
+    },
+    {
+      id: '29ed7b5d-3c95-4bf8-93ef-917ce46a3547',
+      name: 'Sandwich',
+      preparation_time: 74,
+      thumbnail:
+        'https://www.cobsbread.com/drive/uploads/2018/02/Rainbow-Sandwich-1024-x-512-1280x720.jpg',
+    },
+    {
+      id: '51248abf-280e-48cc-a2b5-68f8a48b4dc1',
+      name: 'Hot Dog',
+      preparation_time: 98,
+      thumbnail:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Hotdog_-_Evan_Swigart.jpg/1200px-Hotdog_-_Evan_Swigart.jpg',
+    },
+    {
+      id: '31184c78-5031-445c-9b9f-95850903f5c0',
+      name: 'Lays',
+      preparation_time: 49,
+      thumbnail:
+        'https://images-na.ssl-images-amazon.com/images/I/81TLFU5Yj6L._SY879_.jpg',
+    },
+    {
+      id: '7dc36e2b-a5e1-42a5-a59a-6d4ba3690347',
+      name: 'Doritos',
+      preparation_time: 9,
+      thumbnail:
+        'https://products3.imgix.drizly.com/ci-doritos-nacho-cheese-91d8cf9f375a6fd1.jpeg?auto=format%2Ccompress&ch=Width%2CDPR&fm=jpg&q=20',
+    },
+    {
+      id: '5fe5aea2-7cd3-4ec5-9f27-c97bc2910bed',
+      name: 'Oreos',
+      preparation_time: 6,
+      thumbnail: 'https://images.heb.com/is/image/HEBGrocery/001617462',
+    },
+    {
+      id: 'f2971a22-bc63-4f10-a05a-08ecedd6cd3a',
+      name: 'Minute Maid - Pink Lemonade',
+      preparation_time: 28,
+      thumbnail: 'https://images.heb.com/is/image/HEBGrocery/001554048',
+    },
+    {
+      id: '6ca831a0-b4db-43da-a3da-a8531429846c',
+      name: 'Coffee',
+      preparation_time: 60,
+      thumbnail:
+        'https://images.ctfassets.net/4zu8gvmtwqss/3j9pxyAN1USu4ratAqKtgB/2ead7f784e143a0fd3070a9d9639c078/Coffee-cup-bean-eyes-DIY-395x392.jpg?fm=jpg&fl=progressive&w=1440',
+    },
+    {
+      id: 'a7c36e34-b7db-420c-99fc-5010da66f223',
+      name: 'Water',
+      preparation_time: 15,
+      thumbnail: 'https://veohops.com/wp-content/uploads/2016/09/45.jpg',
+    },
+    {
+      id: '53ec3c05-02a8-484a-9f5a-d30ac7471c66',
+      name: 'Coca-Cola',
+      preparation_time: 58,
+      thumbnail:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/15-09-26-RalfR-WLC-0098.jpg/170px-15-09-26-RalfR-WLC-0098.jpg',
+    },
+    {
+      id: 'bd42b68b-530a-4331-82d7-4fe382abdb77',
+      name: '7up',
+      preparation_time: 2,
+      thumbnail: 'https://sc04.alicdn.com/kf/UTB8mxgiyqrFXKJk43Ovq6ybnpXaX.jpg',
+    },
+    {
+      id: 'c927e462-d160-4a5d-8a3f-2e857a5b484c',
+      name: 'Ocean Spray Cranberry Juice',
+      preparation_time: 95,
+      thumbnail:
+        'https://images-na.ssl-images-amazon.com/images/I/81ARHIFCZ6L._SL1500_.jpg',
+    },
+  ],
+};
