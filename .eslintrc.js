3692183723c06a81b978227a2d3d977a20f741a2
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-06 22:01:29 -0600

    chore: format ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 module.exports = {
-  env: { browser: true, es2021: true, node: true },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   extends: [
     'next',
     'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: { jsx: true },
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 12,
     project: ['./tsconfig.json', './tsconfig.eslint.json'],
     sourceType: 'module',
@@ -23,5 +30,9 @@ module.exports = {
       { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
     ],
   },
-  settings: { react: { version: 'detect' } },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
