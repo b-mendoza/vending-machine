feed959e6cd159699463929abedfbcb0150449e9
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-08 03:35:42 -0600

    style: update sorting of `resets` elements

--- theme/resets.ts
+++ theme/resets.ts
@@ -2,16 +2,16 @@ import { css } from '@emotion/react';
 
 import { Breakpoints } from 'typings/shared';
 
+export const layoutSizeReset = css`
+  margin: 0 auto;
+
+  max-width: 140rem;
+`;
+
 export const paddingReset = css`
   padding: 2rem;
 
   @media (min-width: ${Breakpoints.MobileBreakpoint}) {
     padding: 3rem;
   }
-`;
-
-export const layoutSizeReset = css`
-  margin: 0 auto;
-
-  max-width: 140rem;
 `;
