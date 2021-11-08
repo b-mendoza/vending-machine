585fc4b8b4105d6dd025c2dcd7b8f086857488cd
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-07 23:33:17 -0600

    feat: add types to props of `StyledContainer` styled component

--- theme/shared.ts
+++ theme/shared.ts
@@ -2,12 +2,25 @@ import styled from '@emotion/styled';
 
 import { Breakpoints } from 'typings/shared';
 
-export const StyledContainer = styled.main`
+type StyledContainerProps = {
+  centerContent?: boolean;
+};
+
+export const StyledContainer = styled.main<StyledContainerProps>`
   padding: 2rem;
 
   margin: 0 auto;
 
   max-width: 140rem;
+
+  ${({ centerContent }) =>
+    centerContent
+      ? {
+          display: 'grid',
+          height: '100vh',
+          placeContent: 'center',
+        }
+      : null}
 
   @media (min-width: ${Breakpoints.MobileBreakpoint}) {
     padding: 3rem;
@@ -21,3 +34,5 @@ export const StyledMachineWrapper = styled.div`
 
   grid-template-columns: repeat(auto-fill, minmax(24rem, 1fr));
 `;
+
+export const StyledControlPanel = styled.section``;
