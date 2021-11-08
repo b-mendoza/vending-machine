5bb6f1d3dd4600ef03d1a1b05d6e8354e3e9e12b
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-08 00:35:49 -0600

    feat: add theme resets

---
+++ theme/resets.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+
+import { Breakpoints } from 'typings/shared';
+
+export const paddingReset = css`
+  padding: 2rem;
+
+  @media (min-width: ${Breakpoints.MobileBreakpoint}) {
+    padding: 3rem;
+  }
+`;
+
+export const layoutSizeReset = css`
+  margin: 0 auto;
+
+  max-width: 140rem;
+`;
